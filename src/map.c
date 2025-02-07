1551b389766cae2abf594a62b0c729656c4c6b7e
Kraknes <erling.heimstad.willassen@gmail.com>
2025-02-07 13:58:58 +0100

    fungerende insert

--- src/map.c
+++ src/map.c
@@ -12,6 +12,7 @@
 struct node{
     void *key;
     void *value;
+    size_t key_size;
     struct node *next;
 };
 typedef struct node h_node;
@@ -20,15 +21,15 @@ struct map_t{
     cmp_fn cmpfn;
     hash64_fn hashfn;
     size_t length;
-    int capacity;
-    h_node *head;
-    struct node **hashtables;
+    size_t capacity;
+    struct node **hashtables; 
 };
 
 map_t *map_create(cmp_fn cmpfn, hash64_fn hashfn) {
     // Allokere minne til hashmap
     struct map_t *map = malloc(sizeof(struct map_t));
 
+    // Hvis mappen ikke blir allokert ordentlig
     if (map == NULL){
         printf("ERROR: Failed to malloc map_t in *map_create");
         return 0;
@@ -40,15 +41,16 @@ map_t *map_create(cmp_fn cmpfn, hash64_fn hashfn) {
     map->capacity = SIZE_CAPACITY;
 
     // allokere minne til hash table * capacity (antatt antall på tilførsel)
-    map->hashtables = malloc(sizeof(h_node)*map->capacity);
+    map->hashtables = malloc(map->capacity * sizeof(h_node *));
 
+    // Hvis hashtable ikke får allokert minne ordentlig
     if (map->hashtables == NULL){
-        printf("ERROR: Failed to malloc map->hashtable in *map_create");
+        printf("\nERROR: Failed to malloc map->hashtable in *map_create");
         return 0;
     }
 
     // setter alt av innhold til null for mer effetiv sjekk om index er tom
-    for (int i = 0; i < map->capacity; i++)
+    for (size_t i = 0; i < map->capacity; i++)
     {
         map->hashtables[i] = NULL;
     }
@@ -57,13 +59,28 @@ map_t *map_create(cmp_fn cmpfn, hash64_fn hashfn) {
 
 void map_destroy(struct map_t *map, free_fn val_freefn) {
     
-    for (size_t i = 0; i < map->length; i++)
+    for (size_t i = 0; i < map->capacity; i++)
     {   
-        h_node *tmp = map->head;
-        map->head = map->head->next;
-        val_freefn(tmp);
-        tmp = NULL;
+        if (map->hashtables[i] == NULL)
+        {
+            continue;
+        }
+        else
+        {
+            h_node *tmp = map->hashtables[i]->next;
+            while (map->hashtables[i] != NULL)
+            {
+                val_freefn(map->hashtables[i]->value);
+                free(map->hashtables[i]);
+                map->hashtables[i] = tmp;
+                tmp = tmp->next;
+
+            }
+            free(tmp);
+        }
     }
+    free(map->hashtables);
+    map->hashtables = NULL;
     free(map);
     map = NULL;
 }
@@ -73,39 +90,61 @@ size_t map_length(struct map_t *map) {
 }
 
 void *map_insert(struct map_t *map, void *key, size_t key_size, void *value) {
-    /* TODO LIST:
-    1. Lage node av key og value
-    x. Må returnere 0 hvis alt gikk bra, eller returnere pointer til gamle key hvis det har blitt kollisjon
-    x. Finne ut av key_size, ka e funskjonen    
-    */
-
     // lager node for entry i hash table
     h_node *node = malloc(sizeof(h_node));
 
+    // Hvis noe galt skjer med allokering av minne
     if (node == NULL){
-        printf("ERROR: Failed to malloc h_node in *map_insert");
-        return NULL;
+        printf("\nERROR: Failed to malloc h_node in *map_insert");
     }
 
+    // Setter inn variabler
     node->key = key;
     node->value = value;
+    node->key_size = key_size;
+
 
     // lager en hashed key av nøkkelen
     uint64_t hashed_key = map->hashfn(key);
 
     // modolu av array størrelse for å få unik index til array
-    int hashed_index = hashed_key % map->capacity;
+    long long unsigned hashed_index = hashed_key % map->capacity;
 
-    while (map->hashtables[hashed_index] != NULL)
-    {
-        return map->hashtables[hashed_index];
+    // kollisjonsfunksjon med lenket liste i hashmap. Hvis det er noe allerede i index, går det til denne funksjon
+    if (map->hashtables[hashed_index] != NULL) 
+    {   
+        h_node *tmp = map->hashtables[hashed_index];
+        // Hvis første instans av noden er samme key, så returneres verdien av "gamle key"
+        if(map->cmpfn(tmp->key, node->key) == 0)
+        {
+            return tmp->value;
+        }
+
+        // Sjekker next node om det er samme key, ellers iterere videre i lenketliste
+        while (tmp->next != NULL)
+        {
+            if (map->cmpfn(tmp->next->key, node->key) == 0)
+            {
+                return tmp->next->value;
+            }
+            else
+            {
+                tmp = tmp->next;
+            }
+        }
+        // Hvis det itereres og ikke funnet key, blir den lagt til som *next i siste node i listen
+        tmp->next = node; 
+        map->length++;
+        return NULL;
+        
     }
-    
-    map->hashtables[hashed_index] = node;
-
-    // øker "lengden"/antall medlemmer av array
-    map->length++;
-    return 0;
+    else
+    {
+        map->hashtables[hashed_index] = node;
+        // øker "lengden"/antall medlemmer av array
+        map->length++;
+        return NULL;
+    }  
 }
 
 void *map_remove(struct map_t *map, void *key) {
@@ -117,16 +156,20 @@ void *map_remove(struct map_t *map, void *key) {
     uint64_t hashed_key = map->hashfn(key);
 
     // modolu av array størrelse for å få unik index til array
-    int hashed_index = hashed_key % map->capacity;
+    long long unsigned hashed_index = hashed_key % map->capacity;
     
     // Hvis keyen ikke eksistere i hash tablen
     if (map->hashtables[hashed_index] == NULL){
-        printf("ERROR: No entry in this hash key in *map_remove");
+        printf("\nERROR: No entry in this hash key in *map_remove");
         return 0;
     }
     // henter ut verdien i hashtable, frigjør noden og returnere verdien
+    // OBS - VIl ikke rt_value aldri bli nullifisert? Memory overflow?
+
     void *rt_value = map->hashtables[hashed_index]->value;
     free(map->hashtables[hashed_index]);
+    map->hashtables[hashed_index] = NULL;
+    map->length--;
     return rt_value;
 }
 
@@ -135,15 +178,14 @@ void *map_get(struct map_t *map, void *key) {
     uint64_t hashed_key = map->hashfn(key);
 
     // modolu av array størrelse for å få unik index til array
-    int hashed_index = hashed_key % map->capacity;
+    long long unsigned hashed_index = hashed_key % map->capacity;
 
     if (map->hashtables[hashed_index] == NULL){
-        printf("ERROR: No entry in this hash key in *map_get");
+        printf("\nERROR: No entry in this hash key in *map_get");
         return 0;
     }
 
     void *rt_value = map->hashtables[hashed_index]->value;
-    free(map->hashtables[hashed_index]);
     return rt_value;
 }
 
